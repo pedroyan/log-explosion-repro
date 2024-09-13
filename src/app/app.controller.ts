531ebe1f48286a2448d7406b082204a27eec0789
Pedro Yan Ornelas <pedroyane@gmail.com>
2024-09-13 15:10:11 -0300

    Code

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request as Req } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getData() {
+  getData(@Request() req: Req) {
+    req.log?.info(`This is an info logger, showing the logger is working as expected`);
     return this.appService.getData();
+  }
+
+  @Get('boom')
+  kaboom(@Request() req: Req) {
+    // Generate 12 MB worth of random characters efficiently
+    const randomChars = Array.from({ length: 1024 * 1024 * 12 }, () => Math.random().toString(36)[2]).join('');
+
+    req.log?.info(`I should literally take this container down and that is not ideal :(`, { randomChars });
+    return 'Kaboom!';
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,38 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import * as gcpLogging from '@google-cloud/logging-bunyan';
 import { AppModule } from './app/app.module';
+import { Request as Req } from 'express';
+import BunyanLog = require('bunyan');
+
+declare global {
+  namespace Express {
+    interface Request {
+      // Added to express request type definition since the GCP Logging middleware attaches the logger
+      // to the request object
+      log?: BunyanLog;
+    }
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  // [KEY DEMO] Attach middleware to the express app
+  const { mw, logger } = await gcpLogging.express.middleware({
+    logName: 'logName',
+    level: 'debug',
+  });
+  app.use(mw);
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
